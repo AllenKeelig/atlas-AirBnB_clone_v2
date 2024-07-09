d7076578d1e87e806b42a713b5b4029b361627a6
AllenKeelig <allen.keeling@atlasschool.com>
2024-07-09 17:04:40 -0500

    2

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+'''A simple Flask web application.'''
+from flask import Flask
+
+app = Flask(__name__)
+'''The Flask application instance.'''
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def index():
+    '''The home page.'''
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hbnb():
+    '''The hbnb page.'''
+    return 'HBNB'
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    return "C {}".format(text.replace("_", " "))
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')

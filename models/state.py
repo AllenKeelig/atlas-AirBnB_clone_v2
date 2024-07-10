5b3f894d9adaa4b6b6511bac91f9e276e383c868
AllenKeelig <allen.keeling@atlasschool.com>
2024-07-09 22:36:53 -0500

    new state

--- models/state.py
+++ models/state.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 import models
+import os
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
@@ -13,15 +14,20 @@ class State(BaseModel, Base):
     """State class"""
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="state", cascade="delete")
-
-    @property
-    def cities(self):
-        """Getter for cities based on the storage type"""
-        if STO_TYP == "db":
-            storage = models.storage
-            city_objects = storage.all("City")
-            filtered_cities = [city for city in city_objects.values() if city.state_id == self.id]
-            return filtered_cities
-        else:
-            return []
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        cities = relationship(
+            'City',
+            cascade='all, delete, delete-orphan',
+            backref='state'
+        )
+    else:
+        @property
+        def cities(self):
+            """Returns the cities in this State"""
+            from models import storage
+            cities_in_state = []
+            for value in storage.all(cities_in_state).values():
+                if value.state_id == self.id:
+                    cities_in_state.append(value)
+            return cities_in_state
